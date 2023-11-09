08e249ae6907ee381058992f23ed1cb902193dd3
Nick DeMarco <nicktoriousbig2013@gmail.com>
2023-11-09 12:53:30 -0600

    Add ability to pick new teams

--- team_selector.py
+++ team_selector.py
@@ -10,22 +10,28 @@ players = ['Martin', 'Craig', 'Sue',
 
 print('Welcome to the team allocation tool!')
 
-random.shuffle(players)
+while True:
 
-# Form 1st Team
-team1 = players[:len(players)//2]
+    random.shuffle(players)
 
-print('Team 1 Captain: ' + random.choice(team1))
+    # Form 1st Team
+    team1 = players[:len(players)//2]
 
-print('Team 1:')
-for player in team1:
-    print(player)
+    print('Team 1 Captain: ' + random.choice(team1))
 
-# Form 2nd Team
-team2 = players[len(players)//2:]
+    print('Team 1:')
+    for player in team1:
+        print(player)
 
-print('\nTeam 2 Captain: ' + random.choice(team2))
+    # Form 2nd Team
+    team2 = players[len(players)//2:]
 
-print('Team 2:')
-for player in team2:
-    print(player)
+    print('\nTeam 2 Captain: ' + random.choice(team2))
+
+    print('Team 2:')
+    for player in team2:
+        print(player)
+
+    response = input('\nPick teams again? (Type y or n): ')
+    if response == 'n':
+      break
