178ba5f4d03fddb2129e6c35bf14858b0b0f5cee
Nick DeMarco <nicktoriousbig2013@gmail.com>
2023-11-09 12:49:44 -0600

    Pick teams

--- team_selector.py
+++ team_selector.py
@@ -11,3 +11,21 @@ players = ['Martin', 'Craig', 'Sue',
 print('Welcome to the team allocation tool!')
 
 random.shuffle(players)
+
+# Form 1st Team
+team1 = players[:len(players)//2]
+
+print('Team 1 Captain: ' + random.choice(team1))
+
+print('Team 1:')
+for player in team1:
+    print(player)
+
+# Form 2nd Team
+team2 = players[len(players)//2:]
+
+print('\nTeam 2 Captain: ' + random.choice(team2))
+
+print('Team 2:')
+for player in team2:
+    print(player)
