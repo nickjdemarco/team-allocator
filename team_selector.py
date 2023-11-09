337dd9fcbce4516d8d71aaa3fdfe1e4adfc7e4ca
Nick DeMarco <nicktoriousbig2013@gmail.com>
2023-11-09 12:42:07 -0600

    Add main file and create team

---
+++ team_selector.py
@@ -0,0 +1,13 @@
+import random
+
+players = ['Martin', 'Craig', 'Sue',
+           'Claire', 'Dave', 'Alice',
+           'Luciana', 'Harry', 'Jack',
+           'Rose', 'Lexi', 'Maria',
+           'Thomas', 'James', 'William',
+           'Ada', 'Grace', 'Jean',
+           'Marissa', 'Alan']
+
+print('Welcome to the team allocation tool!')
+
+random.shuffle(players)
